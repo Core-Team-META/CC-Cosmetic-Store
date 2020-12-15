65ba58cc425d158fdd9ee17ccdb83007d2bce88d
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2020-12-15 13:40:38 -0800

    Perks store up and running.

--- Data/Scripts/StoreScript.lua
+++ Data/Scripts/StoreScript.lua
@@ -9,10 +9,18 @@ while not _G.PERKS do
 end
 
 local subscriptionPerk = _G.PERKS.SUBSCRIPTION
+local currencyPerk = _G.PERKS.PREMIUM_CURRENCY
 
 local propAutosavePurchases = propStoreRoot:GetCustomProperty("AutosavePurchases")
 local propAutosaveCurrency = propStoreRoot:GetCustomProperty("AutosaveCurrency")
 local propKeepSubscriptionCosmetics = propStoreRoot:GetCustomProperty("KeepSubscriptionCosmetics")
+
+
+local propCurrencyResourceName = propStoreRoot:GetCustomProperty("CurrencyResourceName")
+local propCurrencyPerPerkPurchase = propStoreRoot:GetCustomProperty("CurrencyPerPerkPurchase")
+local propPremiumCurrencyName = propStoreRoot:GetCustomProperty("PremiumCurrencyName")
+local propSubscriptionOneTimeReward = propStoreRoot:GetCustomProperty("SubscriptionOneTimeReward")
+local propSubscriptionOneTimePremiumReward = propStoreRoot:GetCustomProperty("SubscriptionOneTimePremiumReward")
 
 local playerOwnedCosmetics = {}
 local playerOwnedSubscriptionCosmetics = {}
@@ -89,7 +97,7 @@ function BuyCosmetic(player, templateId, isPartOfSubscription, cost)
 	if player and not Object.IsValid(player) or not player then
 		return
 	end
-	print("Setting: " .. templateId)
+	--print("Setting: " .. templateId)
 
 	player:SetResource("COSMETIC_" .. templateId, 1)
 
@@ -121,6 +129,7 @@ end
 function OnPlayerJoined(player)
 	--print(player.name .. " is VIP?: " .. tostring(subscriptionPerk)))
 	LoadOwnedCosmeticsAndMoney(player)
+	player.perkChangedEvent:Connect(UpdatePlayerPremiums)
 end
 
 function OnPlayerLeft(player)
@@ -188,6 +197,55 @@ function LoadOwnedCosmeticsAndMoney(player)
 			end
 		end
 	end
+end
+
+function UpdatePlayerPremiums(player, perkRef)
+
+    if perkRef ~= subscriptionPerk and perkRef ~= currencyPerk then
+    
+        return
+        
+    end
+
+    local data = Storage.GetPlayerData(player)
+    
+    if data.COSMETICS == nil then
+    
+        data.COSMETICS = {}
+        
+    end
+        
+    if data.COSMETICS.currencyGranted == nil then
+    
+        data.COSMETICS.currencyGranted = 0
+        
+    end
+    
+    local currencyPurchased = player:GetPerkCount(currencyPerk)
+    local currentPremiumCurrency = player:GetResource(propPremiumCurrencyName)
+        
+    if currencyPurchased > data.COSMETICS.currencyGranted then
+        currentPremiumCurrency = currentPremiumCurrency + propCurrencyPerPerkPurchase * (currencyPurchased - data.COSMETICS.currencyGranted)
+        data.COSMETICS.currencyGranted = currencyPurchased
+    end
+    
+    if not data.COSMETICS.subscribedBefore and player:HasPerk(subscriptionPerk) then
+        data.COSMETICS.subscribedBefore = true
+        
+        local currentCurrency = player:GetResource(propCurrencyResourceName)
+        currentCurrency = currentCurrency + propSubscriptionOneTimeReward
+        player:SetResource(propCurrencyResourceName, currentCurrency)
+        
+        currentPremiumCurrency = currentPremiumCurrency + propSubscriptionOneTimePremiumReward
+    end
+    
+    player:SetResource(propPremiumCurrencyName, currentPremiumCurrency)
+    
+    local SaveData = Storage.GetPlayerData(player)
+    SaveData = data
+    print(SaveData.COSMETICS.subscribedBefore)
+    
+    Storage.SetPlayerData(player, SaveData)
 end
 
 function CheckSubscription(player)
